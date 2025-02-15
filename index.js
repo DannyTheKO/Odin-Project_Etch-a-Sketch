55f3d98c724af8a25a871ab4749d9547ca1e7950
Danny <thanhan1100@gmail.com>
2025-02-16 01:30:26 +0700

    add event listener script

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log('Happy developing ✨')
+const etchASketch = document.querySelector("#etchASketch");
+const btnReset = document.querySelector("#btnReset");
+const btnSize = document.querySelector("#btnSize");
+
+etchASketch.addEventListener('mouseover', (e) => {
+    if (e.target.id !== "etchASketch") {
+
+    }
+})
+
+// Reset Button
+btnReset.addEventListener('click', (e) => {
+
+})
+
+// Change Size
+btnSize.addEventListener('click', () => {
+    let size;
+
+    // Validation input, if not an integer, try again!
+    do {
+        size = prompt("What size for the sketch?: ");
+    } while (isNaN(parseInt(size)));
+});
+
