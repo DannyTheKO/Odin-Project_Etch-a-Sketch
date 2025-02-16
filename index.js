503bc09fd7c279cb8ff2cc4c9a17bacaef8e9fd0
Danny <thanhan1100@gmail.com>
2025-02-16 12:30:35 +0700

    Added Grid Sketch

--- index.js
+++ index.js
@@ -2,6 +2,35 @@ const etchASketch = document.querySelector("#etchASketch");
 const btnReset = document.querySelector("#btnReset");
 const btnSize = document.querySelector("#btnSize");
 
+let totalGrid = 128; // default size (contain x and y axis);
+const containerSize = 600; // Size of the container
+let count = 1; // Debug
+renderSketch(totalGrid);
+function renderSketch(totalGrid) {
+    let gridSize = Math.ceil(Math.sqrt(totalGrid));
+    let divSize = containerSize / gridSize;
+    console.log(divSize)
+
+    etchASketch.style.display = "grid";
+    etchASketch.style.gridTemplateColumns = `repeat(${gridSize}, ${divSize}px)`;
+    etchASketch.style.gridTemplateRows = `repeat(${gridSize}, ${divSize}px)`;
+    etchASketch.style.width = `${containerSize}px`;
+    etchASketch.style.height = `${containerSize}px`;
+
+    etchASketch.innerHTML = "";
+
+    for (let i = 0; i < totalGrid; i++) {
+        // Sketch square default style
+        const square = document.createElement("div");
+        square.style.backgroundColor = "darkblue";
+        square.style.boxSizing = "border-box"; // Ensure consistent sizing
+        square.style.width = `${divSize}px`;
+        square.style.height = `${divSize}px`;
+
+        etchASketch.appendChild(square);
+    }
+}
+
 etchASketch.addEventListener('mouseover', (e) => {
     if (e.target.id !== "etchASketch") {
 
@@ -15,11 +44,11 @@ btnReset.addEventListener('click', (e) => {
 
 // Change Size
 btnSize.addEventListener('click', () => {
-    let size;
-
     // Validation input, if not an integer, try again!
     do {
-        size = prompt("What size for the sketch?: ");
-    } while (isNaN(parseInt(size)));
+        totalGrid = prompt("What size for the sketch?: ");
+    } while (isNaN(parseInt(totalGrid)));
+
+    renderSketch(totalGrid);
 });
 
